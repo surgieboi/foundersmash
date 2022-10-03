b5cbff9370c0f1c8dd41a73b8fe6f5028ce9ae79
Sergio Villasenor <42443963+surgieboi@users.noreply.github.com>
2022-10-03 09:47:42 +0300

    updated the inclusion of google analytics

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,11 +1,23 @@
 import Footer from '../footer/footer'
 import Header from '../header/header'
 import Meta from '../meta/meta'
+import Script from 'next/script'
 
 export default function Layout({ children }) {
     return (
         <>
             <Meta></Meta>
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+                        ga('create', 'UA-238969693-3', 'auto');
+                        ga('send', 'pageview');
+                    `}
+            </Script>
             <div className="h-screen flex flex-col">
                 <Header></Header>
                 {children}
--- components/meta/meta.js
+++ components/meta/meta.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Script from 'next/script'
 
 export default function Meta() {
     return (
@@ -17,17 +16,6 @@ export default function Meta() {
                 <meta name="twitter:description" content="Pass or Smash on Top Founders" />
                 <meta name="twitter:image" content="https://foundersmash.vercel.app/og.jpg" />
                 <meta name="twitter:card" content="summary_large_image"></meta>
-                <Script id="google-analytics" strategy="afterInteractive">
-                    {`
-                        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-                        ga('create', 'UA-238969693-3', 'auto');
-                        ga('send', 'pageview');
-                    `}
-                </Script>
             </Head>
         </>
     )
