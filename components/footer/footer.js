4887cc19b91517f21e42a5de5a6f44e1a4a7ea8b
Sergio Villasenor <42443963+surgieboi@users.noreply.github.com>
2022-10-04 10:31:21 +0300

    added product hunt

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -8,12 +8,12 @@ export default function Footer() {
                         flex mx-auto justify-between content-between items-center
                         border-t border-pink-200">
             <p className="w-1/2">
-                <Link href="http://www.producthunt.com" passHref>
-                    <a className="ml-1 underline" target="_blank">
-                        Product Hunt
-                    </a>
-                </Link>
-            </p>
+                <Link href="https://www.producthunt.com/posts/foundersmash?utm_source=badge-featured&#38;utm_medium=badge&#38;utm_souce=badge-foundersmash" passHref>
+                        <a target="_blank">
+                            <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=362069&#38;theme=neutral" alt="FounderSmash | Product Hunt" style={{width: '250px', height: '54px'}} />
+                        </a>
+                    </Link>
+                </p>
             <p className="w-1/2 text-right">
                 Made with <span className="text-red-500">&#10084;</span> from 🇪🇪
             </p>
