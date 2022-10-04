6d70f5bace05f6f8897ab7f5bbb97eb070fcfce5
Sergio Villasenor <42443963+surgieboi@users.noreply.github.com>
2022-10-04 17:03:59 +0300

    updated metadata

--- components/meta/meta.js
+++ components/meta/meta.js
@@ -4,8 +4,8 @@ export default function Meta() {
     return (
         <>
             <Head>
-                <title>FounderSmash | Pass or Smash on Top Founders</title>
-                <meta name="description" content="FounderSmash | Pass or Smash on Top Founders" />
+                <title>FounderSmash | Pass 🙅 or smash 😈 on Silicon Valley&#39;s top founders!</title>
+                <meta name="description" content="FounderSmash | Pass 🙅 or smash 😈 on Silicon Valley&#39;s top founders!" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
                 <link rel="icon" href="/favicon.png" />
                 <meta property="og:title" content="FounderSmash" />
@@ -13,9 +13,12 @@ export default function Meta() {
                 <meta property="og:url" content="https://foundersmash.vercel.app/" />
                 <meta property="og:image" content="https://foundersmash.vercel.app/og.jpg" />
                 <meta name="twitter:title" content="FounderSmash" />
-                <meta name="twitter:description" content="Pass or Smash on Top Founders" />
+                <meta name="twitter:description" content="Pass 🙅 or smash 😈 on Silicon Valley&#39;s top founders!" />
                 <meta name="twitter:image" content="https://foundersmash.vercel.app/og.jpg" />
-                <meta name="twitter:card" content="summary_large_image"></meta>
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta property="og:image:alt" content="Pass 🙅 or smash 😈 on Silicon Valley&#39;s top founders!"></meta>
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
             </Head>
         </>
     )
